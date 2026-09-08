2716f32fef8d18e5b6bddd096a0d669dcd3a1c2e
shafi-shaik-666 <shafibjr666@gmail.com>
2026-09-08 07:15:08 +0530

    Package installation

---
+++ 01-mongodb.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+LOGS_DIR="/var/log/roboshop"
+sudo mkdir -p $LOGS_DIR
+sudo chown -R ec2-user:ec2-user $LOGS_DIR
+sudo chmod -R 755 $LOGS_DIR
+LOGS_FILE="$LOGS_DIR/$0.log"
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
+
+if [ $USERID -ne 0 ]; then
+    echo -e "$TIMESTAMP [Error] $R Run this Script with root priviliges $N" | tee -a $LOGS_FILE
+    exit 1
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e "$TIMESTAMP $2 .. $R Failure $N" | tee -a $LOGS_FILE
+        exit 1
+    else 
+        echo -e "$TIMESTAMP $2 .. $G INFO $N" | tee -a $LOGS_FILE
+    fi
+}
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+VALIDATE $? "Adding mongo repo"
+
+
